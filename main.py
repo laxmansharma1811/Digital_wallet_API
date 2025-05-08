947b9a65a246a7b9d2e491807b90701c6e0e1645
laxmansharma1811 <laxmansharma1811@gmail.com>
2025-05-08 17:11:49 +0545

    SessionLocal added in main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from controllers import auth_controller, wallet_controller, transaction_controller, tier_controller
-from db.database import engine
+from db.database import engine, SessionLocal
+from services.tier_service import initialize_tiers
 from models import user, wallet
 from db.database import Base
 
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
+
+with SessionLocal() as db:
+    initialize_tiers(db)
 
 app = FastAPI(title="Digital Wallet API")
 
